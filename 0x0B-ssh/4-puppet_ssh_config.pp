a885cc45dbc2cea781e62fc96629279e25a144b6
Cris-Mur <yomy98@gmail.com>
2020-08-04 17:49:45 -0500

    [add] ssh project work 200

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# manifest modify the file ssh_config
+
+file_line { 'no_password':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'private_key':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentifyFile ~/.ssh/holberton',
+}
