25443c8a9944015bbc84ef939d488fada3e64ebc
Cris-Mur <yomy98@gmail.com>
2020-08-18 16:18:00 -0500

    [try fix]

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,12 @@
 # manifest modify the file ssh_config
+file_line { 'private_key':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentifyFile ~/.ssh/holberton',
+  replace => true,
+}
 
 file_line { 'no_password':
   path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
-  replace => true,
-}
-
-file_line { 'private_key':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    IdentifyFile ~/.ssh/holberton',
   replace => true,
 }
