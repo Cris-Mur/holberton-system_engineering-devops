b2791ed7c60cfc7817450e114619887bc4db0ed9
Cris-Mur <yomy98@gmail.com>
2020-08-31 15:09:45 -0500

    [try6]

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,5 @@
 # using scripts in puppet
 exec {'./0-custom_http_response-header':
-  command => "/usr/bin/bash 'apt-get -y update; apt-get -y install nginx; echo "Holberton School" >  /var/www/html/index.nginx-debian.html; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me google.com permanent;\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-enabled/default; service nginx start'"
+  provider => shell,
+  command  => 'apt-get -y update; apt-get -y install nginx; echo "Holberton School" >  /var/www/html/index.nginx-debian.html; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me google.com permanent;\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-enabled/default; service nginx start'
 }
