9574f5e3a4f1b2d1e80a5e625be2e0eae55588a1
Cris-Mur <yomy98@gmail.com>
2020-08-31 14:45:21 -0500

    [try 2]

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,8 @@
 # using scripts in puppet
 exec {'./0-custom_http_response-header':
-  command => '/usr/bin/bash -c ./0-custom_http_response-header'
+  command => "/usr/bin/bash -c '0-custom_http_response-header'"
 }
 
 exec {'1-install_load_balancer':
-  command => '/usr/bin/bash -c ./1-install_load_balancer'
+  command => "/usr/bin/bash -c '1-install_load_balancer'"
 }
